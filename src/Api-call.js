c832b9ce11abbc619c479fcaf36d9dad99fefa7b
Mary Bruff <marybruff5@gmail.com>
2023-12-01 14:05:18 -0700

    add logic to get on click functionality to work on single poster

--- src/Api-call.js
+++ src/Api-call.js
@@ -7,12 +7,31 @@ export const retrieveData = async () => {
     const data = await response.json()
     console.log('API Data:', data)
     return(data)
-     
+  
   } catch (error) {
     if (error.message === 'Failed to fetch') {
       throw new Error('Network error: Please check your internet connection')
     }
     console.error(error)
     throw error 
+  }
+}
+
+export const singleMovieId = async (id) => {
+  try {
+    const response = await fetch( `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    if (!response.ok) {
+      throw new Error("Oops! Something went wrong")
+    }
+    const data = await response.json()
+    console.log('Single Movie Data:', data)
+    return(data)
+
+  } catch (error) {
+    if (error.message === 'Failed to fetch') {
+      throw new Error('Network error: Please check your internet connection')
+    }
+    console.error(error)
+    throw error
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
-import { useState, useEffect } from 'react'
-import Header from './components/Header/Header'
-import Movies from './components/Movies/Movies'
-import movieData  from './movieData'
-import { retrieveData } from './Api-call'
+import { useState, useEffect } from 'react';
+import Header from './components/Header/Header';
+import Movies from './components/Movies/Movies';
+import movieData  from './movieData';
+import { retrieveData, singleMovieId } from './Api-call';
+import Modal from "./components/Modal/Modal";
+
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
   const [apiMovieData, setApiMovieData] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [error, setError] = useState('');
+  const [movieId, setMovieId] = useState(''); 
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     retrieveData()
@@ -23,10 +27,25 @@ function App() {
     setApiMovieData(apiMovieData)
   }, [])
 
+  const handleMovieClick = async (id) => {
+    setMovieId(id);
+    try {
+      const movieDetails = await singleMovieId(id);
+      setSelectedMovie(movieDetails); 
+      setModalIsOpen(true);
+    } catch (error) {
+      setError(error.message || 'An unknown error occurred.')
+    }
+  }
+  
   return (
     <div className="App">
       <Header />
-      <Movies movies={movies} apiMovieData={apiMovieData}/>
+      {!modalIsOpen ? (
+        <Movies handleMovieClick={handleMovieClick} movies={movies} apiMovieData={apiMovieData}/>
+      ) : (
+        <Modal selectedMovie={selectedMovie} setModalIsOpen={setModalIsOpen} />
+      )}
     </div>
   );
 }
