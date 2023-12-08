d0ba359841cf32a267ba97e58905b70ad5ca4e5e
Mary Bruff <marybruff5@gmail.com>
2023-12-08 14:10:09 -0700

    ensure all tests are passing and add tests to check for image in modal and error

--- cypress/e2e/movies_spec.cy.js
+++ cypress/e2e/movies_spec.cy.js
@@ -4,31 +4,42 @@ describe("Movies Homepage", () => {
     cy.intercept(
       "GET",
       "https://rancid-tomatillos.herokuapp.com/api/v2/movies/",
-      { fixture: "allMoviesData.json" },
+      { fixture: "allMoviesData.json" }
     ).as("getAllMovies");
   });
 
   it("Should display a header", () => {
-    cy.get('.movie-header').should('be.visible')
-      .get('.tomatillo-icon').should('be.visible')
-      .get('h1').should('contain', 'Rancid Green Tomatillos')
-      .get('.header-nav').should('be.visible')
-      .get('.nav-button').should('be.visible')
+    cy.get(".movie-header")
+      .should("be.visible")
+      .get(".tomatillo-icon")
+      .should("be.visible")
+      .get("h1")
+      .should("contain", "Rancid Green Tomatillos")
+      .get(".header-nav")
+      .should("be.visible")
+      .get(".nav-button")
+      .should("be.visible");
   });
 
   it("Should display a movie card", () => {
-    cy.get('.movie-card').should('be.visible')
-      .get('.movie-image').should('be.visible')
-      .get('.movie-title').should('be.visible')
-      .get('.star-rating').should('be.visible')
+    cy.get(".movie-card")
+      .should("be.visible")
+      .get(".movie-image")
+      .should("be.visible")
+      .get(".movie-title")
+      .should("be.visible")
+      .get(".star-rating")
+      .should("be.visible");
   });
-  
+
   it("Should display all movies", () => {
-    cy.get('.movie-card').should('have.length', 40)
+    cy.get(".movie-card").should("have.length", 40);
   });
 
   it("Should filter movies by title", () => {
-    cy.get('.search-input').type('The')
-      .get('.movie-card').should('have.length', 9)
+    cy.get(".search-input")
+      .type("The")
+      .get(".movie-card")
+      .should("have.length", 9);
   });
 });
